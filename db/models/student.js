cc6af9373a61e32361536e23b9670c62780b1e98
Jenny Zeng <jzeng7077@gmail.com>
2020-11-22 15:27:16 -0500

    student model solutions added

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,28 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('Student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+});
+
+Student.beforeCreate((student) => {
+  student.firstName = student.firstName[0].toUpperCase() + student.firstName.slice(1)
+  student.lastName = student.lastName[0].toUpperCase() + student.lastName.slice(1)
+})
 
 module.exports = Student;
